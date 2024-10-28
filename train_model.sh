e27047ad0aab37863fdd2f9aa860ea5517b3b768
Dami <damion@spiti.co.za>
2024-10-28 19:22:41 +0200

    Plotting experiments

--- train_model.sh
+++ train_model.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export QT_QPA_PLATFORM=offscreen
+export LD_LIBRARY_PATH=/usr/lib/wsl/lib:$LD_LIBRARY_PATH
+ldconfig -p | grep cuda
 
 # Variables
 iterations=5000
@@ -10,7 +13,7 @@ latent_dist="gaussian"  # Or "concrete"
 batch_size=256
 num_segments=3
 demo_file="trajectories/colours/15k_layered"
-save_dir="runs/colours/15k_colours_5ke_plots"
+save_dir="runs/colours/15k_colours_5ke_plots_new_2"
 # save_dir="runs/colours/test2"
 random_seed=42
 train_model=true
